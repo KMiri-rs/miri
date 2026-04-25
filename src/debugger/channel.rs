f06a71fe430aa1cf25713d064a64a293ccd774e8
zjp <jiping_zhou@foxmail.com>
2026-04-25 17:33:49 +0000

    debugger: refactor crossterm event by making it a channel msg

--- src/debugger/channel.rs
+++ src/debugger/channel.rs
@@ -1,11 +1,18 @@
 use std::sync::mpsc::{self, Receiver, Sender};
 
+use crossterm::event::Event;
+
 use super::{DebuggerCommand, DebuggerState};
 
-pub type StateSender = Sender<DebuggerState>;
-pub type StateReceiver = Receiver<DebuggerState>;
+pub type StateSender = Sender<StateOrEvent>;
+pub type StateReceiver = Receiver<StateOrEvent>;
 pub type CommandSender = Sender<DebuggerCommand>;
 pub type CommandReceiver = Receiver<DebuggerCommand>;
+
+pub enum StateOrEvent {
+    State(DebuggerState),
+    Event(Event),
+}
 
 pub fn state_channel() -> (StateSender, StateReceiver) {
     mpsc::channel()
