69cf03430ebb6b72d74fc3657dfefb0a533bd5f7
zjp <jiping_zhou@foxmail.com>
2026-05-31 21:38:44 +0800

    debugger(locals): hide dead locals via `D`

--- src/debugger/tui/pane/locals.rs
+++ src/debugger/tui/pane/locals.rs
@@ -13,33 +13,44 @@ impl PaneLocals {
         PaneLocals { rect, ..Default::default() }
     }
 
-    pub fn widget(&self, state: &DebuggerState, focus: bool, stack_index: usize) -> Table<'static> {
+    pub fn widget(
+        &self,
+        state: &DebuggerState,
+        focus: bool,
+        stack_index: usize,
+        no_dead: bool,
+    ) -> Table<'static> {
         let selected_locals = state
             .stack_frames
             .get(stack_index)
             .map(|f| f.locals.as_slice())
             .unwrap_or_else(|| state.locals.as_slice());
 
-        let rows = selected_locals.iter().skip(self.scroll.into()).map(|local| {
-            let value_style = match local.state {
-                LocalKind::Dead => Style::default().fg(THEME_DIM),
-                LocalKind::Uninitialized =>
-                    Style::default().fg(THEME_ERR).add_modifier(Modifier::BOLD),
-                LocalKind::Pointer => Style::default().fg(THEME_WARN).add_modifier(Modifier::BOLD),
-                LocalKind::Initialized => Style::default().fg(THEME_OK),
-            };
-            let name_style = if local.state == LocalKind::Dead {
-                Style::default().fg(THEME_DIM)
-            } else {
-                Style::default().fg(THEME_ACCENT_SOFT)
-            };
-            Row::new([
-                Cell::from(local.idx.clone()).style(name_style),
-                Cell::from(local.name.clone()).style(name_style),
-                Cell::from(local.ty.clone()).style(Style::default().fg(THEME_DIM)),
-                Cell::from(hscroll_text(&local.value, self.hscroll)).style(value_style),
-            ])
-        });
+        let rows = selected_locals
+            .iter()
+            .filter(|local| if no_dead { local.value != "-" } else { true }) // filter out dead locals
+            .skip(self.scroll.into())
+            .map(|local| {
+                let value_style = match local.state {
+                    LocalKind::Dead => Style::default().fg(THEME_DIM),
+                    LocalKind::Uninitialized =>
+                        Style::default().fg(THEME_ERR).add_modifier(Modifier::BOLD),
+                    LocalKind::Pointer =>
+                        Style::default().fg(THEME_WARN).add_modifier(Modifier::BOLD),
+                    LocalKind::Initialized => Style::default().fg(THEME_OK),
+                };
+                let name_style = if local.state == LocalKind::Dead {
+                    Style::default().fg(THEME_DIM)
+                } else {
+                    Style::default().fg(THEME_ACCENT_SOFT)
+                };
+                Row::new([
+                    Cell::from(local.idx.clone()).style(name_style),
+                    Cell::from(local.name.clone()).style(name_style),
+                    Cell::from(local.ty.clone()).style(Style::default().fg(THEME_DIM)),
+                    Cell::from(hscroll_text(&local.value, self.hscroll)).style(value_style),
+                ])
+            });
 
         Table::new(
             rows,
