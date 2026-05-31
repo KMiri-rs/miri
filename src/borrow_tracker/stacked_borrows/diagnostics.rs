a6a91bc1b2fe77d382354d3fde671bc854c14ed8
zjp <jiping_zhou@foxmail.com>
2026-05-31 21:12:42 +0800

    debugger(borrow_stacks): fix span and source code rendering
    close https://github.com/KMiri-rs/KMiri/issues/44

--- src/borrow_tracker/stacked_borrows/diagnostics.rs
+++ src/borrow_tracker/stacked_borrows/diagnostics.rs
@@ -262,7 +262,7 @@ impl AllocHistory {
             id,
             root: (
                 item,
-                machine.current_user_relevant_span(),
+                machine.debugger_current_span(),
                 machine.debugger_fn_name(),
                 machine.debugger_body_span(),
             ),
