7e3579bba3184f86988792e5fc69bb169308d3ea
zjp <jiping_zhou@foxmail.com>
2026-07-24 16:37:18 +0800

    debugger: add `t` for run-to-terminator

--- src/concurrency/scheduler.rs
+++ src/concurrency/scheduler.rs
@@ -224,11 +224,21 @@ pub trait EvalContextExt<'tcx>: crate::MiriInterpCxExt<'tcx> {
                     if let Some(handle) = &this.machine.debugger {
                         let stack = this.active_thread_stack();
                         if !stack.is_empty() {
-                            let state = DebuggerState::capture(this);
-                            handle.send(state);
-                            if handle.wait_for_continue() == DebuggerCommand::Quit {
-                                this.machine.handle_abnormal_termination();
-                                throw_machine_stop!(TerminationInfo::Interrupted);
+                            handle.send(this);
+
+                            match handle.wait_for_continue(this) {
+                                DebuggerCommand::Quit => {
+                                    this.machine.handle_abnormal_termination();
+                                    throw_machine_stop!(TerminationInfo::Interrupted)
+                                }
+                                DebuggerCommand::QuitWithErr(err) => {
+                                    use std::io::Write;
+                                    let file = std::fs::File::create("miri-debugger.log").unwrap();
+                                    writeln!(&file, "Debugger exited with error:\n{err}").unwrap();
+                                    this.machine.handle_abnormal_termination();
+                                    throw_machine_stop!(TerminationInfo::Interrupted)
+                                }
+                                _ => (),
                             }
                         }
                     }
