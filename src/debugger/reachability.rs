4f7beefccc7755c41b07c7bbce5634edbed4cfdc
zjp <jiping_zhou@foxmail.com>
2026-08-04 15:39:30 +0800

    debugger: rename ENV_INNER_DIR_TARGET to ENV__KMIRI_DIR_TARGET

--- src/debugger/reachability.rs
+++ src/debugger/reachability.rs
@@ -3,7 +3,7 @@ use kmiri_helper::*;
 pub type Reachability = std::sync::Arc<[FunctionInstanceInfo]>;
 
 pub fn collect() -> Option<Reachability> {
-    let dir_target = std::env::var(ENV_INNER_DIR_TARGET).ok()?;
+    let dir_target = std::env::var(ENV__KMIRI_DIR_TARGET).ok()?;
     let dir_target = std::path::PathBuf::from(dir_target);
     let analysis_json = dir_target.join(ANALYSIS_JSON);
     let file = std::fs::File::open(analysis_json).ok()?;
